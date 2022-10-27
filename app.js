4734d206c7131c68ab9dec3dcb675ce102f74afb
Boworanan <bowonranan.30@gmail.com>
2022-10-27 16:32:35 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            message: "Hello World!",
+            fname: "แหน๋น",
+            count: 0,
+        }
+    },
+}).mount("#app")
